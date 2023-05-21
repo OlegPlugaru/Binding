d536a52f2a5afd4dce7986d2cc2b0f16869a021b
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-21 19:30:52 +0300

    Initial Commit

---
+++ Binding/BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingApp.swift
+//  Binding
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
